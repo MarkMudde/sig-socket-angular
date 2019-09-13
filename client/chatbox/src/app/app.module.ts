e4275a75889da1c6d9bafd577c5b17cf7c9d71af
Mark Mudde <mark.mudde@amis.nl>
2019-09-13 11:16:57 +0200

    Without private chat

--- client/chatbox/src/app/app.module.ts
+++ client/chatbox/src/app/app.module.ts
@@ -7,24 +7,17 @@ import { AppComponent } from './app.component';
 import { HandlerService } from './services/handler.service';
 import { ChatComponent } from './chat/chat.component';
 import { LoginComponent } from './login/login.component';
-import { PrivateChatComponent, PrivateChatPopUpComponent } from './chat/private-chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    LoginComponent,
-    PrivateChatComponent,
-    PrivateChatPopUpComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
-  ],
-  entryComponents: [
-    PrivateChatComponent,
-    PrivateChatPopUpComponent
   ],
   providers: [
     HandlerService
